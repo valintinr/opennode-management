edd4fd7b53f57192e07dd73ee22d9f69df6f883b
Administrator <root@localhost.localdomain>
2015-09-23 11:59:19 +0300

    transaction 1.4.4

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "setuptools", # Redundant but removes a warning
         "Twisted==13.1.0",
         "pyasn1==0.1.2", # required by Twisted conch
-        "transaction==1.3.0",
+        "transaction",
         "zope.component==3.12",
         "zope.app.catalog==3.8.1",
         "zope.app.intid==3.7.1",
